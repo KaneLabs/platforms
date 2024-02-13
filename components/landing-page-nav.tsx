6e440c1515d2188ef231a32d48d387e53be4f4f5
Alexander Nicita <alexandernicita@gmail.com>
2024-02-13 10:41:26 -0500

    great catch ryan, thank you sir

--- components/landing-page-nav.tsx
+++ components/landing-page-nav.tsx
@@ -19,9 +19,9 @@ export default function LandingPageNav() {
 
   return (
     <div>
-      <nav className="fixed left-0 right-0 top-10 z-550 mx-auto h-14 overflow-clip  border-b border-gray-500/10">
+      <nav className="fixed left-0 right-0 top-10 mx-auto h-14 overflow-clip border-b border-gray-500/10">
         <div className={`${styles["landing-page-nav-backdrop"]}`}>
-          <div className="mx-auto flex h-14 w-full max-w-5xl items-center  justify-between px-5">
+          <div className="mx-auto flex h-14 w-full max-w-5xl items-center justify-between px-5">
             <Link
               href={"/"}
               className="font-serif text-xl font-light text-gray-300"
@@ -45,20 +45,22 @@ export default function LandingPageNav() {
           </div>
         </div>
       </nav>
-    <div className="h-10 py-2 px-2 w-full bg-gray-900 border-b border-gray-500/10 font-medium items-center text-brand-primary/75">
-      <Link
-        href={"https://explorer.gitcoin.co/?utm_source=grants.gitcoin.co&utm_medium=internal_link&utm_campaign=gg19&utm_content=independent-rounds#/round/10/0xd875fa07bedce182377ee54488f08f017cb163d4"}
-        target="_blank"
-      >
-        <Marquee
-          pauseOnHover={true}
-          speed={100}
-          className={"w-full"}
+      <div className="fixed h-10 py-2 px-2 w-full bg-gray-900 border-b border-gray-500/10 font-medium items-center text-brand-primary/75">
+        <Link
+          href={"https://explorer.gitcoin.co/?utm_source=grants.gitcoin.co&utm_medium=internal_link&utm_campaign=gg19&utm_content=independent-rounds#/round/10/0xd875fa07bedce182377ee54488f08f017cb163d4"}
+          target="_blank"
         >
+          <Marquee
+            pauseOnHover={true}
+            speed={100}
+            className={"w-full overflow-clip z-550"}
+          >
+            Fora is on Gitcoin for Zuzalu Q1 Tech Round! Your small donation helps catalyze more startup cities 
           Fora is on Gitcoin for Zuzalu Q1 Tech Round! Your small donation helps catalyze more startup cities
-        </Marquee>
-      </Link>
-    </div>
+            Fora is on Gitcoin for Zuzalu Q1 Tech Round! Your small donation helps catalyze more startup cities 
+          </Marquee>
+        </Link>
+      </div>
     </div>
   );
 }
