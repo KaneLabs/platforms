c56009fc6995dc22acc92a4234eb8e1f2626e75f
anljz <161668560+anljz@users.noreply.github.com>
2024-03-29 19:09:22 -0700

    Subtext

--- app/app/(dashboard)/city/[subdomain]/campaigns/[id]/settings/[segment]/[editType]/layout.tsx
+++ app/app/(dashboard)/city/[subdomain]/campaigns/[id]/settings/[segment]/[editType]/layout.tsx
@@ -5,11 +5,17 @@ import { notFound, redirect } from "next/navigation";
 import CampaignSettingsNav from "./nav";
 import { userHasOrgRole } from "@/lib/actions";
 
+const subTextDict: { [key: string]: string; } = {
+  basic: "Start with the basic information of your Campaign",
+  tiers: "Set your contribution tiers",
+  details: "Set parameters for how you want contributions to be made"
+}
+
 export default async function CampaignSettingsLayout({
   params,
   children,
 }: {
-  params: { subdomain: string, editType: string };
+  params: { subdomain: string, editType: string, segment: string };
   children: ReactNode;
 }) {
   const session = await getSession();
@@ -38,10 +44,13 @@ export default async function CampaignSettingsLayout({
 
   return (
     <>
-      <div className="flex flex-col items-center space-x-4 space-y-2 sm:flex-row sm:space-y-0">
-        <h1 className="text-xl dark:text-white sm:text-3xl">
+      <div className="flex flex-col items-start space-y-2">
+        <h1 className="font-serif text-3xl text-gray-800 font-semibold dark:text-white">
           {params.editType === "create" ? "Create Campaign" : "Edit Campaign"} 
         </h1>
+        <h4 className="truncate rounded-md bg-gray-100 py-1 font-medium text-gray-600 transition-colors dark:bg-gray-800 dark:text-gray-400">
+          {subTextDict[params.segment]}
+        </h4>
       </div>
       <CampaignSettingsNav />
       {children}
