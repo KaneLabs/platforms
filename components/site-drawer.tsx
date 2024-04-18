884371754a5a3632a94743ed6da37d361f875f1e
anljz <161668560+anljz@users.noreply.github.com>
2024-04-17 18:39:54 -0700

    Nav border color

--- components/site-drawer.tsx
+++ components/site-drawer.tsx
@@ -267,7 +267,7 @@ export default function Drawer({ children }: { children: ReactNode }) {
               </a>
             ))}
           </div>
-          <div className="my-2 border-t border-gray-200 dark:border-gray-700" />
+          <div className="my-2 border-t border-gray-300 dark:border-gray-700" />
           {children}
         </div>
       </DrawerPaper>
