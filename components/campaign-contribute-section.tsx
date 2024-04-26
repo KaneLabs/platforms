22a61c276af269fc12ad8f22de96d426ca1c0a7b
anljz <161668560+anljz@users.noreply.github.com>
2024-04-25 17:58:58 -0700

    Update campaign contribute section component

--- components/campaign-contribute-section.tsx
+++ components/campaign-contribute-section.tsx
@@ -3,34 +3,79 @@
 import { Button } from "./ui/button";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
-import { CampaignWithData } from "@/lib/actions";
 import { getCurrencySymbol } from "@/lib/utils";
+import { Campaign, FinancialVisibilityType } from "@prisma/client";
+
+const defaultAmount = 0;
+const defaultTarget = 5000;
 
 interface CampaignContributeSectionProps {
-  campaign: CampaignWithData;
+  campaign: Partial<Campaign>;
   isDeadlineExceeded: boolean;
+  totalContribution?: number | null;
   className: string;
+  visibility?: FinancialVisibilityType | null;
+  isPublic?: boolean;
 }
 
 export default function CampaignContributeSection({
   campaign,
   isDeadlineExceeded,
+  totalContribution,
   className,
+  visibility,
+  isPublic
 }: CampaignContributeSectionProps) {
 
   const router = useRouter();
 
+  if (visibility === FinancialVisibilityType.BUTTON_ONLY) {
+    return (
+      <div className={`flex flex-col items-center justify-center ${className}`}>
+        <Button
+          onClick={() => isPublic && router.push(`${campaign.id}/checkout/tiers`)}
+          className="w-full rounded-full"
+          disabled={isDeadlineExceeded}
+        >
+          {isDeadlineExceeded ? "Ended" : "Fund"}
+        </Button>
+      </div>
+    );
+  }
+
+  if (visibility === FinancialVisibilityType.TARGET_ONLY) {
+    return (
+      <div className={`flex flex-col ${className}`}>
+        <div>
+          <div className="text-left text-2xl font-semibold">
+            {getCurrencySymbol(campaign.currency)}{campaign.threshold || defaultTarget} {campaign.currency}
+          </div>
+          <div className="text-left text-sm">Target raise</div>
+        </div>
+        <div className="mt-4">
+          <Button
+            onClick={() => isPublic && router.push(`${campaign.id}/checkout/tiers`)}
+            className="w-full rounded-full"
+            disabled={isDeadlineExceeded}
+          >
+            {isDeadlineExceeded ? "Ended" : "Fund"}
+          </Button>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className={`flex flex-col ${className}`}>
       <div>
-        <div className="text-2xl font-semibold">
-          {getCurrencySymbol(campaign.currency)}{campaign.threshold} {campaign.currency}
+        <div className="text-left text-2xl font-semibold">
+          {getCurrencySymbol(campaign.currency)}{totalContribution || defaultAmount} {campaign.currency}
         </div>
-        <div className="text-sm">Minimum Target</div>
+        <div className="text-left text-sm">Raised of {getCurrencySymbol(campaign.currency)}{campaign.threshold || defaultTarget} {campaign.currency} target</div>
       </div>
       <div className="mt-4">
         <Button
-          onClick={() => router.push(`${campaign.id}/checkout/tiers`)}
+          onClick={() => isPublic && router.push(`${campaign.id}/checkout/tiers`)}
           className="w-full rounded-full"
           disabled={isDeadlineExceeded}
         >
